6407bcad5195c8ea76349e2d2c145b5e28dcb4aa
Chánh Phạm <73011753+chanh12012001@users.noreply.github.com>
2022-03-16 22:34:42 +0700

    v1.0.6

--- config/sms.config.js
+++ config/sms.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     ACCOUNT_SID: "AC2e6f71651c15ff91345267975b462fde",
-    AUTH_TOKEN : "67105dae8fa7923ce70a2e3a70552cdd",
+    AUTH_TOKEN : "e26c64d82b1cb9274bc0aba378ebc5a9",
 }
