d9d9bf3313fbe3cec20d830eed2aa0c68dc8dd86
Ho Quang <19520875@gm.uit.edu.vn>
2022-03-18 16:04:46 +0700

    spending_db_v.1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require('./routes/user.routes')
 const routesTask = require('./routes/task.routes')
+const routesAlbum = require('./routes/album.routes')
 const routesSpending = require('./routes/spending.routes')
 const app = express()
 
@@ -10,5 +11,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/', routes)
 app.use('/', routesTask)
 app.use('/', routesSpending)
+app.use('/', routesAlbum)
 
 module.exports = app
